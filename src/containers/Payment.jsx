ae137b485922f47640daac93a8685366ea759533
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-24 11:28:31 -0500

    Payment/Styles add - Success/Styles add ⚖

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 const Payment = () => {
     return (
-    <div>Payment</div>
+    <div className="Payment">
+        <div className="Payment-content">
+            <h3>Resumen de tu pedido:</h3>
+            <div className="Payment-button">
+                Boton de pago de paypal
+            </div>
+        </div>
+    </div>
     );
 }
 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
     return (
-    <div>Success</div>
+    <div className="Success">
+        <div className="Success-content">
+            <h2>Sneyder, Gracias por tu compra</h2>
+            <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+            <div className="Success-map">
+                Google Maps
+            </div>
+        </div>
+    </div>
     );
 }
 
